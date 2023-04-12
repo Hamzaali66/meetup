28352673c2e4e2a14cea3ea92a55640224d8ba0f
Hamza <hamza@inspedium.com>
2023-04-12 14:10:26 +0500

    first post API create

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,5 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { MongoClient } from 'mongodb'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {  //POST api of new meetup page
+    const data = req.body
+    console.log(data, "data--")
+    // const { title, image, address, description } = data
+    
+    //this link we get from mongodb database to connect with app
+    const client = await MongoClient.connect('mongodb+srv://hamza:24nwO2kwsepHBms9@cluster1.runvqoc.mongodb.net/meetups?retryWrites=true&w=majority')
+    const db = client.db()
+    const meetupCollection = db.collection('meetups')
+
+    const result = await meetupCollection.insertOne(data)
+    console.log(result, "resilt----")
+    client.close() //to close the db once done
+
+    res.status(201).json({ message: "meetup insert successfully!" })
+  }
 }
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,19 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 
 
-export default function NewMeetUpPage(){
+export default function NewMeetUpPage() {
 
-    function addMeetupHandler(enterMeetupData){
-        console.log(enterMeetupData, "helooooo12")
+    async function addMeetupHandler(enterMeetupData) {
+        // console.log(enterMeetupData, "helooooo12")
+        const response = await fetch('/api/new-meetup', {
+            method: "POST",
+            body: JSON.stringify(enterMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data, "responseDATA")
     }
 
     return <NewMeetupForm onAddMeetup={addMeetupHandler} />
